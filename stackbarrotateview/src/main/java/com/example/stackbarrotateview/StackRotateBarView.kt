0d7804e2f864b84a5a4b077e370edf1a6f230ac9
Anwesh43 <anweshthecool0@gmail.com>
2021-05-15 00:31:03 +0530

    adding Canvas extension methofs

--- stackbarrotateview/src/main/java/com/example/stackbarrotateview/StackRotateBarView.kt
+++ stackbarrotateview/src/main/java/com/example/stackbarrotateview/StackRotateBarView.kt
@@ -32,3 +32,28 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawStackRotateBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val rw : Float = Math.min(w, h) / recWFactor
+    val rh : Float = Math.min(w, h) / recHFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    save()
+    translate(w / 2, 0f)
+    for (j in 0..(bars - 1)) {
+        val sfj : Float = sf.divideScale(j + 1, parts)
+        save()
+        translate(0f, rh / 2 + (h - rw / 2 - rh / 2) * sfj)
+        rotate(rot * sfj)
+        drawRect(RectF(-rw / 2, -rh / 2, rw / 2, -rh / 2 + rh * sf1), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSRBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawStackRotateBar(scale, w, h, paint)
+}
