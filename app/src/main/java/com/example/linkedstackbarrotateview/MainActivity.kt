a836efef75b68c2fde68657229878756a0b96d0f
Anwesh43 <anweshthecool0@gmail.com>
2021-05-15 00:49:48 +0530

    adding fullscreen support and view in activity

--- app/src/main/java/com/example/linkedstackbarrotateview/MainActivity.kt
+++ app/src/main/java/com/example/linkedstackbarrotateview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedstackbarrotateview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.stackbarrotateview.StackRotateBarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        StackRotateBarView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
