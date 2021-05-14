4f6cc80aa73e2a103a1ea6a8e0eb8fd62222e6d1
Anwesh43 <anweshthecool0@gmail.com>
2021-05-15 00:32:47 +0530

    adding StackRotateBarView class

--- stackbarrotateview/src/main/java/com/example/stackbarrotateview/StackRotateBarView.kt
+++ stackbarrotateview/src/main/java/com/example/stackbarrotateview/StackRotateBarView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawSRBNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawStackRotateBar(scale, w, h, paint)
 }
+
+class StackRotateBarView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
